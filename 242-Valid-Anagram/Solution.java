abcd0b7616fb9d881c0adb4d76b153952fe7a92d
Hani Gupta <guptahani935@gmail.com>
2025-12-18 01:11:18 +0530

    Add solution code for Valid Anagram

--- 242-Valid-Anagram/Solution.java
+++ 242-Valid-Anagram/Solution.java
@@ -1,1 +1,24 @@
+class Solution {
+    
+    public static boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()) return false;
+         char[] sArray =s.toCharArray();
+            char[] tArray =t.toCharArray();
+            Arrays.sort(sArray);
+            Arrays.sort(tArray);
+            return Arrays.equals(sArray,tArray);
+    }
+    public static void main(String[]arg){
+     String s = "listen";
+        String t = "silent";
+
+         
+        if(isAnagram(s,t)){
+            System.out.println("true");
+        }
+        else{
+            System.out.println("false");
+        }
+    }
+}
 
