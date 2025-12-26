099dabf27188dd75c7a39f03c4195dbbd6bfa528
Hani Gupta <guptahani935@gmail.com>
2025-12-27 01:42:37 +0530

    Add files via upload

---
+++ 26-Remove-Duplicates-from-Sorted-Array/Solution.java
@@ -0,0 +1,14 @@
+﻿class Solution {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) return 0;
+
+        int count = 1; // index for placing unique elements
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[count] = nums[i];
+                count++;
+            }
+        }
+        return count;
+    }
+}
